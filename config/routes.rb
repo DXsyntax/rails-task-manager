c1508fbff7e997c61890ce9fded1d71f93384f85
Dexter Goh <bboifatt@gmail.com>
2020-03-15 02:14:00 +0800

    added route for show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index" # route for listing all task
+  get "tasks/:id", to: "tasks#show" # route for listing ONE specific task, althou i still have no idea how this works
 end
+
+
