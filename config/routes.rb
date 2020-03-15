cc26233c50ec53d8d50080382083471f01b6a06d
Dexter Goh <bboifatt@gmail.com>
2020-03-15 11:18:46 +0800

    fixed the anchor url path for my show path using association and task[:id] key

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index" # route for listing all task
-  get "tasks/:id", to: "tasks#show" # route for listing ONE specific task, althou i still have no idea how this works
+  get "tasks/:id", to: "tasks#show", as: :taskid# route for listing ONE specific task, althou i still have no idea how this works, my association allows me to name prefixes for routes
 end
 
 
