2f713e8f8615dd6663607352e2a7be448b2fa315
Dexter Goh <bboifatt@gmail.com>
2020-03-15 14:39:11 +0800

    added in my form for in edit view file but does not work

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index" # route for listing all task
   get "tasks/new", to: "tasks#new"  # this is the part where i have to put new and create above id? or it will read new as a unique id too, but i cant recall why
   post "tasks", to: "tasks#create", as: :taskcreate # this is for submitting the form , confirmation that association is for setting our prefixes, but still not understanding the logic behind it
-  get "tasks/:id", to: "tasks#show", as: :taskid# route for listing ONE specific task, althou i still have no idea how this works, my association allows me to name prefixes for routes
   get "tasks/:id/edit", to: "tasks#edit", as: :taskedit # route for edit , get request
+  get "tasks/:id", to: "tasks#show", as: :taskid# route for listing ONE specific task, althou i still have no idea how this works, my association allows me to name prefixes for routes
   #route for update , patch request
 end
 
